88a996d6220a9a93c3ea043af38f73f8ba66dfc1
Artem Kartasov <art.kartasov@gmail.com>
2017-01-28 22:43:46 +0700

    Leanplum client. Send messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package main
+package leanplum
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -13,7 +14,6 @@ import (
 
 const (
 	Leanplum_api_url = "https://www.leanplum.com/api"
-	configFile       = "config.toml"
 )
 
 type Config struct {
@@ -29,30 +29,35 @@ type CommonResponse struct {
 type Response struct {
 	Success bool           `json:"success"`
 	Error   *ResponseError `json:"error,omitempty"`
+	Warning *ResponseError `json:"warning,omitempty"`
+	Result  *string        `json:"result,omitempty"`
+	// MessagesSent *string        `json:"messagesSent,omitempty"`
 }
 
 type ResponseError struct {
 	Message string `json:"message"`
 }
 
-func main() {
-	query := ReadConfig()
-	params := map[string]string{
-		"action":         "setUserAttributes",
-		"userAttributes": "{\"Interests\":[\"Go\",\"IT\"]}",
-	}
-	Get(query, params)
+type Message struct {
+	Data MessageContent `json:"data"`
+}
+
+type MessageContent struct {
+	Time   int64             `json:"time"`
+	Values map[string]string `json:"values"`
 }
 
 func Get(config Config, arguments map[string]string) {
-	auth, _ := query.Values(config)
+	uriParams, _ := query.Values(config)
 	queryString := url.Values{}
 	for k, v := range arguments {
 		queryString.Add(k, v)
 	}
-	url := Leanplum_api_url + "?" + auth.Encode() + queryString.Encode()
+	url := Leanplum_api_url + "?" + uriParams.Encode() + queryString.Encode()
 
 	response, err := http.Get(url)
+
+	defer response.Body.Close()
 
 	resp := new(CommonResponse)
 	err = json.NewDecoder(response.Body).Decode(resp)
@@ -68,21 +73,65 @@ func Get(config Config, arguments map[string]string) {
 	log.Printf("Success is %v\n", resp.Response[0].Success)
 }
 
-func ReadConfig() Config {
+func Post(config Config, body []byte, parameters map[string]string) {
+	uriParams, _ := query.Values(config)
+	queryString := makeQueryString(parameters)
+	url := Leanplum_api_url + "?" + uriParams.Encode() + "&" + queryString.Encode()
+
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+
+	if response != nil {
+		defer response.Body.Close()
+	}
+
+	if err != nil {
+		log.Fatalf("%v\n", err)
+		return
+	}
+
+	resp := new(CommonResponse)
+	err = json.NewDecoder(response.Body).Decode(resp)
+
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+		return
+	}
+
+	if !resp.Response[0].Success {
+		if resp.Response[0].Error != nil {
+			log.Fatalf("The error message: %v\n", resp.Response[0].Error.Message)
+			return
+		}
+	}
+
+	if resp.Response[0].Warning != nil {
+		log.Fatalf("The warning message: %v\n", resp.Response[0].Warning.Message)
+		return
+	}
+
+	log.Printf("Success is %v. Result is %v\n", resp.Response[0].Success, resp.Response[0].Result)
+}
+
+// Read config from toml-file
+func ReadConfig(configFile string) Config {
 	_, err := os.Stat(configFile)
 	if err != nil {
 		log.Fatal("Config file is not read: ", configFile)
 	}
 
-	var conf Config
-	if _, err := toml.DecodeFile(configFile, &conf); err != nil {
+	var config Config
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
 		log.Fatal(err)
 	}
 
-	return conf
+	return config
 }
 
-func Call(auth Config, action string, argumetns map[string]string) (bool, error) {
+func makeQueryString(parameters map[string]string) url.Values {
+	queryString := url.Values{}
+	for k, v := range parameters {
+		queryString.Add(k, v)
+	}
 
-	return true, nil
+	return queryString
 }
